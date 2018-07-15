a47e76847ebc5cba24e614b4141a980c5b24d884
Victor <viwilson@calpoly.edu>
2018-07-15 16:24:17 -0700

    Computed all MLE's, began plotting qq-plots

--- 417 R code/Lab 3 Code.R
+++ 417 R code/Lab 3 Code.R
@@ -1,45 +1,57 @@
-##Lab 3 Code
+#Lab 3 code
 
 #MLE's Question 1
+
+rearrest <- read.csv("Data sets/Rearrest.txt", sep = "\t")
 data <- rearrest
+names(data)[names(data) == 'censor'] <- 'Censor'
+names(data)[names(data) == 'months'] <- 'Time'
 
 #Formatting data for fitdist function
 left <- c()
 right <- c()
 
-for (i in 1:length(data$months)) {
-  if (data$censor[i] == 1) {
-    left <- c(left, data$months[i])
-    right <- c(right, data$months[i])
+for (i in 1:length(data$Time)) {
+  if (data$Censor[i] == 1) {
+    left <- c(left, data$Time[i])
+    right <- c(right, data$Time[i])
   } 
   else {
-    left <- c(left, data$months[i])
+    left <- c(left, data$Time[i])
     right <- c(right, NA)
   }
 }
 
 d <- data.frame(left, right)
 
+
 #Exponential distribution parameter estimates
 check <- fitdistcens(d, "exp")
-1/check$estimate
+rate <- 1/check$estimate
+
+library('fitdistrplus')
+plot(fitdist(data$Time,"exp"))
+
 
 #Weibull distribution parameter estimates
 check <- fitdistcens(d, "weibull")
 check$estimate
 
+library(car)
+###qPlot(d, distribution="weibull", shape=0.8279898, scale=26.8386386)
 
 #Lognormal distribution parameter estimates
 check <- fitdistcens(d, "lnorm")
 check$estimate
+##qqPlot(d, distribution="lognormal", shape=2.798188, scale=1.746138)
 
 #Logistic distribution 
 check <- fitdistcens(d, "logis")
 check$estimate
+##qqPlot(d, distribution="logis", location=18.791516, scale=9.784775)
 
 #Loglogistic distribution
-dllogis(x, shape = 1, scale = 1, log = FALSE) 
-
+library(flexsurv)
 check <- fitdistcens(d, "llogis")
 check$estimate
 
