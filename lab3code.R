fd5061271737d5164e1f7e2a1309d149efbf56c8
Ashley J <apjacobs@calpoly.edu>
2018-07-21 15:34:17 -0700

    attempted qqplot

--- lab3code.R
+++ lab3code.R
@@ -2,31 +2,55 @@ library(fitdistrplus)
 library(ADGofTest)
 
 ad <- function(data, dist) {
-  left <- c()
-  right <- c()
-  
-  for (i in 1:length(data$Time)) {
-    if (data$Censor[i] == 1) {
-      left <- c(left, data$Time[i])
-      right <- c(right, data$Time[i])
-    } 
-    else {
-      left <- c(left, data$Time[i])
-      right <- c(right, NA)
-    }
-  }
-  
-  d <- data.frame(left, right)
-  fit <- fitdistcens(d, dist)
-  rate <- fit$estimate[["rate"]]
-  start <- floor(qexp(.01, rate))
-  end <- ceiling(qexp(.99, rate))
+
   ad.test(data$Time, pexp, rate) #is ad test same for censored and complete?
   
 }
+data <- read.csv("Data sets/Rearrest.txt", sep = "\t")
+head(data)
+
 data <- read.csv("Data sets/MELT TIMES V2 W2018.txt", sep = "\t")
-data$Time <- data$T
-data$Censor <- data$C
-ad(data, "exp") #not same as minitab
 
+qqplot <- function(data, dist, time, censor) {
+  
+  fit <- fit_data(data, dist, time, censor) 
+  data <- data[order(data[[time]]),]
+  time <- as.vector(data[[time]])
+  censor <- as.vector(data[[censor]])
+  
+  Percent <- c()
+  x <- c()
+  for (i in 1:length(time)) {
+    if (censor[i] == 1) {
+      x <- c(x, time[i])
+      Percent <- c(Percent, (i / length(time)) * 100)
+    } 
+  }
+  
+  qfunc <- match.fun(paste("q", dist, sep = ""))
+  l <- c(p = .01, fit$estimate)
+  s <- split(unname(l), names(l))
+  l <- c(p = .99, fit$estimate)
+  e <- split(unname(l), names(l))
 
+  start <- floor(do.call(qfunc, s))
+  end <- ceiling(do.call(qfunc, e))
+  
+  df <- data.frame(x, Percent)
+  p <- ggplot(df, aes(x = x, y = Percent)) + geom_point() +
+      #geom_line(data = data.frame(x = c(start, end), y = c(1, 99)), aes(x = x, y = y)) +
+      scale_x_continuous(name = "Time", 
+                         breaks = seq(start, 400, by = (400) / 5), 
+                         limits = c(start, 400)) +
+      scale_y_continuous(name = "Percent", 
+                         breaks = seq(0, 100, by = 20),
+                         limits = c(0, 100)) +
+      ggtitle(paste(dist, "probability plot")) +
+      theme(axis.text.x = element_text(size = rel(1.5)),
+            axis.text.y = element_text(size = rel(1.5)),
+            axis.title.y = element_text(size = rel(1.5)),
+            axis.title.x = element_text(size = rel(1.5)),
+            plot.title = element_text(size = rel(2)))
+  plot(p)
+}
+qqplot(data, "exp", "months", "censor")
