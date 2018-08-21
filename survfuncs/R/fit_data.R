94a4dd68f71bc15ac1ac252e54d22a00a858dfc6
Ashley J <apjacobs@calpoly.edu>
2018-08-21 09:59:26 -0700

    Added by variable to plot_density, minor fixes in help files

--- survfuncs/R/fit_data.R
+++ survfuncs/R/fit_data.R
@@ -9,7 +9,7 @@
 #' @param censor The string name of the censor column of the dataframe. Defaults to "Censor". 
 #' The censor column must be a numeric indicator variable where complete times correspond 
 #' to a value of 1 and incomplete times correspond to 0.
-#' @param by The string name of a grouping variable. if specified, the function returns a list. 
+#' @param by The string name of a grouping variable. If specified, the function returns a list. 
 #' Variable can contain logical, string, character, or numeric data.
 #' 
 #' @examples
--- survfuncs/R/plot_density.R
+++ survfuncs/R/plot_density.R
@@ -2,13 +2,15 @@
 #' Plotting density function overlayed on top of a histogram of data
 #'
 #' Creates histogram of right censored data with the density function of a fitted parametric distribution overlayed.
-#' @param data a dataframe containing a time column and a censor column.
-#' @param dist a string name for a distribution that has a corresponding density function and distribution function.
+#' @param data A dataframe containing a time column and a censor column.
+#' @param dist A string name for a distribution that has a corresponding density function and distribution function.
 #' Examples include "norm", "lnorm", "exp", "weibull", "logis", "llogis", "gompertz", etc.
-#' @param time the string name of the time column of the dataframe. Defaults to "Time".
+#' @param time The string name of the time column of the dataframe. Defaults to "Time".
 #' @param censor The string name of the censor column of the dataframe. Defaults to "Censor". 
 #' The censor column must be a numeric indicator variable where complete times correspond 
 #' to a value of 1 and incomplete times correspond to 0.
+#' @param by The string name of a grouping variable. If specified, the function returns a list. 
+#' Variable can contain logical, string, character, or numeric data.
 #' @import ggplot2 graphics
 #' @examples
 #' library(survival) 
@@ -17,14 +19,35 @@
 #' plot_density(kidney, "weibull", time = "time", censor = "status")
 #' @export
  
-plot_density <- function(data, dist, time = "Time", censor = "Censor") {
+plot_density <- function(data, dist, time = "Time", censor = "Censor", by = "") {
   
+  #if there's a grouping variable
+  if (by != "") {
+    #stores grouping variable as a vector
+    data[[by]] <- as.factor(data[[by]])
+    b <- as.factor(as.vector(data[[by]]))
+    
+    #loops through the levels in the grouping variable
+    for (i in levels(b)) {
+      #subsets dataframe
+      d2 <- data[data[[by]] == i, ]
+      d2[[by]] <- NULL
+      #calls plot_density recursively, adds title
+      plot(plot_density(d2, dist, time, censor) + ggtitle(paste(dist, "distribution,", "level =", i)))
+    }
+  }
+  
+  #fits data
   fit <- fit_data(data, dist, time, censor)
+  #stores density function
   dfunc <- match.fun(paste("d", dist, sep = ""))
+  #restores time as variable t (don't worry about it)
   t <- time
-  
+  #stores arguments for density function
   args <- c(fit$estimate)
   args <- split(unname(args), names(args))
+  
+  #plots density plot
   p <- ggplot(data, aes(x = data[[t]])) +
     geom_histogram(aes_string(y = "..density.."), bins = 30) +
     stat_function(fun = dfunc, args = args, 
@@ -43,5 +66,5 @@ plot_density <- function(data, dist, time = "Time", censor = "Censor") {
           legend.position = c(0.8,0.8))
   
   p 
-  
 }
+
