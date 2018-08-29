d76087e768565e2ec1229db83d89030230c2c640
Shannon Pileggi <skmcclintock@gmail.com>
2018-08-28 19:27:02 -0700

    more work on AD

--- AndersonDarling.R
+++ AndersonDarling.R
@@ -18,9 +18,26 @@ data
 #fits data to distribution
 fit <- fit_data(data, dist, "time", "censor") 
 
+# overall sample size
+n_all <- length(time)
 
-#orders data by time
-data <- data[order(data[["time"]]),]
+# orders data by time and censor
+data <- data[order(data[["time"]], -data[["censor"]]),]
+
+# computes rank of time values
+data$rank <- as.numeric(as.factor(data$time))
+
+
+AR <- 0 
+
+for (i in 1:n_all){
+  AR <- ( (n_all - data$rank[i] + 1)*AR + n_all + 1 ) / (n_all - data$rank[i] + 2)
+  data$AR[i] <- AR
+}
+
+
+
+data
 
 # complete data
 data <- data[data[["censor"]]==1, ]
@@ -29,8 +46,11 @@ data <- data[data[["censor"]]==1, ]
 time <- as.vector(data[["time"]])
 censor <- as.vector(data[["censor"]])
                     
+
+
 calc <- data.frame(time = rep(NA, n+1), 
                            censor = rep(NA, n+1),
+                           AR =  rep(NA, n+1), 
                            z = rep(NA, n+1),
                            Fz = rep(NA, n+1),
                            z_lag = rep(NA, n+1),
@@ -39,14 +59,18 @@ calc <- data.frame(time = rep(NA, n+1),
                            B = rep(NA, n+1),
                            C = rep(NA, n+1))                    
 
-calc$time[1:n] <- time
-calc$censor[1:n] <- censor
+calc$time[1:n] <- data$time
+calc$censor[1:n] <- data$censor
+calc$AR[1:n] <- data$AR
 
 # fitted estimate of cdf
 calc$z[1:n] <- pweibull(time, shape, scale)
 
 # empirical estimate of cdf based on Median Rank method
-calc$Fz[1:n] <- (1:n - 0.3) / (n + 0.4) 
+# I believe the estimates we are trying to match in minitab are
+# 0.147, 0.367, 0.698
+# calc$Fz <- (calc$AR - 0.3) / (n_all + 0.4) 
+calc$Fz <- c(0.147, 0.367, 0.698, NA)
 
 # fixed values
 calc$z[n+1] <-0.999999999999
