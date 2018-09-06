4b2ee345e8bc0c200658519858c849d3636950d8
Ashley J <apjacobs@calpoly.edu>
2018-09-06 13:50:07 -0700

    added by option to surv_prob, minor change in help file for plot_density

--- parmsurvfit/R/plot_density.R
+++ parmsurvfit/R/plot_density.R
@@ -9,7 +9,8 @@
 #' @param censor The string name of the censor column of the dataframe. Defaults to "censor". 
 #' The censor column must be a numeric indicator variable where complete times correspond 
 #' to a value of 1 and incomplete times correspond to 0.
-#' @param by The string name of a grouping variable. If specified, the function returns a list. 
+#' @param by The string name of a grouping variable. If specified, the function plots each group individually along 
+#' with the plot for all groups together. 
 #' Variable can contain logical, string, character, or numeric data.
 #' @import ggplot2 graphics
 #' @examples
